b0e666b9d7fee2d5cfb4438e0fadbc3a68f83422
Baker <eab6863@gmail.com>
2021-05-01 16:30:12 -0400

    Added mention event

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ const app = new App({
   appToken: process.env.APP_TOKEN,
 });
 
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
+
+
 // Listens to incoming messages that contain "hello"
 app.message('hey', async ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
@@ -21,9 +29,31 @@ app.message('hey', async ({ message, say }) => {
   await say(`Hello, <@${message.user}>`);
 });
 
-(async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
-
-  console.log('⚡️ Bolt app is running!');
-})();
+// subscribe to 'app_mention' event in your App config
+// need app_mentions:read and chat:write scopes
+app.event('app_mention', async ({ event, context, client, say }) => {
+  try {
+    await say({"blocks": [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Thanks for the mention <@${event.user}>! Here's a button`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Button",
+            "emoji": true
+          },
+          "value": "click_me_123",
+          "action_id": "first_button"
+        }
+      }
+    ]});
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
