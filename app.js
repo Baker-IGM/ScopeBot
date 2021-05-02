25b39cd1c6d4e8e582c637140c73fe68376b640a
Baker <eab6863@gmail.com>
2021-05-02 16:21:11 -0400

    Copied scope text to json

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+const fs = require('fs');
+
+let rawdata;
+
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.BOT_TOKEN,
@@ -13,9 +17,16 @@ const app = new App({
   appToken: process.env.APP_TOKEN,
 });
 
+//  Start app
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
   console.log('⚡️ Bolt app is running!');
+
+  fs.readFile('data.json', (err, data) => {
+    if (err) throw err;
+    rawdata = JSON.parse(data);
+    console.log(rawdata);
+  });
 })();
